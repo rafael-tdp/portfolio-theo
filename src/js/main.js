8f2373414082f67c986f311674b8b93d08630b5c
Rafael93150 <tavaresrafael93@gmail.com>
2025-04-03 12:53:27 +0200

    add project slider and rename fc into usfood

--- src/js/main.js
+++ src/js/main.js
@@ -80,3 +80,93 @@ window.addEventListener("scroll", () => {
 		});
 	}
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+	const params = new URLSearchParams(window.location.search);
+	const projectName = params.get("nom");
+
+	if (!projectName) {
+		document.getElementById("project-title").innerText =
+			"Projet non trouvé";
+		return;
+	}
+
+	const projects = {
+		minimalist: {
+			title: "Minimalist",
+			images: ["minimalist-1.jpg", "minimalist-2.jpg"],
+		},
+		formicidae: {
+			title: "Formicidae",
+			images: ["formicidae-1.jpg", "formicidae-2.jpg", "formicidae-3.jpg", "formicidae-4.jpg", "formicidae-5.jpg"],
+		},
+		loft: {
+			title: "The Loft",
+			images: ["loft-7.jpg", "loft-8.jpg", "loft-9.jpg", "loft-3.jpg", "loft-4.jpg", "loft-5.jpg", "loft-6.jpg"],
+		},
+		usfood: {
+			title: "USFOOD",
+			images: ["usfood-6.jpg", "usfood-7.jpg", "usfood-8.jpg", "usfood-9.jpg"],
+		},
+		appart: {
+			title: "Appart'Agence",
+			images: ["appart-1.jpg", "appart-2.jpg", "appart-3.jpg", "appart-4.jpg", "appart-5.jpg", "appart-6.jpg"],
+		},
+	};
+
+	function generateProjetSlides(project) {
+		let diapoHtml = "";
+		project.images.forEach((img) => {
+			diapoHtml += `<img src="./public/img/projects/${projectName}/${img}" alt="${project.title}">`;
+		});
+		return diapoHtml;
+	}
+
+	generateProjetSlides(projects[projectName]);
+
+	const project = projects[projectName];
+	if (!project) {
+		document.getElementById("project-title").innerText =
+			"Projet non trouvé";
+		return;
+	}
+
+	document.getElementById("project-title").innerText = project.title;
+	let diapoHtml = "";
+	project.images.forEach((img) => {
+		diapoHtml += `<img src="./public/img/projects/${projectName}/${img}" alt="${project.title}">`;
+	});
+	document.getElementById("diapo").innerHTML += diapoHtml;
+});
+
+let currentSlide = 0;
+
+var slides = document.querySelectorAll("#diapo img");
+var totalSlides = slides.length;
+setTimeout(() => {
+	slides = document.querySelectorAll("#diapo img");
+	totalSlides = slides.length;
+}, 1000);
+
+function showSlide(index) {
+	currentIndex = index;
+	if (!slides[index]) return;
+	const projectWidth = slides[0].offsetWidth;
+	const offset = -index * projectWidth;
+	slides.forEach((project, i) => {
+		project.style.transform = `translateX(${offset}px)`;
+		project.style.transition = "transform 0.5s ease-in-out";
+	});
+}
+
+function nextProjectSlide() {
+	currentSlide = (currentSlide + 1) % totalSlides;
+	showSlide(currentSlide);
+}
+
+function prevProjectSlide() {
+	currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+	showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
